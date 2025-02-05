d50600303da97ad22615bd7ea3cdf9909b702a46
dungle <letiendungdt@gmail.com>
2025-02-05 17:25:20 +0700

    27 using thymeleaf

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/repositories/PublisherRepository.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/repositories/PublisherRepository.java
@@ -1,4 +1,7 @@
 package guru.springframework.spring6webapp.repositories;
 
-public interface PublisherRepository {
+import guru.springframework.spring6webapp.domain.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
 }
