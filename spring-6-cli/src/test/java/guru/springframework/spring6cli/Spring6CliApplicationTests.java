d2c5f3ad6f69d549f21778583eb2d7e37031285e
dungle <letiendungdt@gmail.com>
2025-02-06 10:34:04 +0700

    p3 dependency injection

---
+++ spring-6-cli/src/test/java/guru/springframework/spring6cli/Spring6CliApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6CliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
