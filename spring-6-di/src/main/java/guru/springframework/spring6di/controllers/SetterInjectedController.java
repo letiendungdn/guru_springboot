c259301ab3101262832df588065c722986e5a977
dungle <letiendungdt@gmail.com>
2025-02-07 13:37:48 +0700

    38 using qualifiers

--- spring-6-di/src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ spring-6-di/src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -2,12 +2,26 @@ package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
 
+//    @Qualifier("setterGreetingBean")
+//    @Autowired
+//    private GreetingService greetingService;
+    /// /    @Autowired
+//    public void setGreetingService(@Qualifier("setterGreetingBean") GreetingService greetingService) {
+//        System.out.println("SetterInjectedController.setGreetingService");
+//        this.greetingService = greetingService;
+//    }
+
+//    @Qualifier("setterGreetingBean")
+//    @Autowired
     private GreetingService greetingService;
+
+    @Qualifier("setterGreetingBean")
     @Autowired
     public void setGreetingService(GreetingService greetingService) {
         System.out.println("SetterInjectedController.setGreetingService");
