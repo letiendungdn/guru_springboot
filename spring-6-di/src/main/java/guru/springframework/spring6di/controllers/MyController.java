61d72b94771a330ec282dda8f7f6308a7a93269d
letiendungdt <letiendungdt@gmail.com>
2025-02-07 21:32:00 +0700

    42 spring been lìe cycle demo

--- spring-6-di/src/main/java/guru/springframework/spring6di/controllers/MyController.java
+++ spring-6-di/src/main/java/guru/springframework/spring6di/controllers/MyController.java
@@ -13,9 +13,17 @@ public class MyController {
         this.greetingService = new GreetingServiceImpl();
     }
 
-    public String sayHello() {
+    public String sayHello(){
         System.out.println("I'm in the controller");
 
         return greetingService.sayGreeting();
+    }
+
+    public void beforeInit(){
+        System.out.println("## - Before Init - Called by Bean Post Processor");
+    }
+
+    public void afterInit(){
+        System.out.println("## - After init called by Bean Post Processor");
     }
 }
