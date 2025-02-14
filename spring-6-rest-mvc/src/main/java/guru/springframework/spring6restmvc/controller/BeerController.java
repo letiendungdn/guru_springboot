ab818574629ba751dbf0314057bfc396875832d0
dungle <letiendungdt@gmail.com>
2025-02-14 14:19:55 +0700

    86 using exceptiong handler

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -74,6 +74,12 @@ public class BeerController {
         return beerService.listBeers();
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(){
+        System.out.println("In exception handler");
+        return ResponseEntity.notFound().build();
+    }
+
 //    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
     @GetMapping(value = BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
