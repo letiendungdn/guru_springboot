0d44508fd2443c57f7d5279aec0ec60097aecae7
letiendungdt <letiendungdt@gmail.com>
2025-02-15 23:14:56 +0700

    101 jpa get operations

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/services/BeerServiceJPA.java
@@ -3,7 +3,6 @@ package guru.springframework.spring6restmvc.services;
 import guru.springframework.spring6restmvc.mappers.BeerMapper;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
-import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Service
 @Primary
 @RequiredArgsConstructor
@@ -21,21 +24,25 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> listBeers() {
-        return List.of();
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(beerMapper.beerToBeerDto(beerRepository.findById(id)
+                .orElse(null)));
     }
 
     @Override
-    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+    public BeerDTO saveNewBeer(BeerDTO beer) {
         return null;
     }
 
     @Override
-    public void updateBeerById(UUID beerId, BeerDTO beerDTO) {
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
 
     }
 
@@ -45,7 +52,7 @@ public class BeerServiceJPA implements BeerService {
     }
 
     @Override
-    public void patchBeerById(UUID beerId, BeerDTO beerDTO) {
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
 
     }
 }
