40e35fac9a80c72647d2c982337846f6810f1cd9
letiendungdt <letiendungdt@gmail.com>
2025-02-16 20:31:46 +0700

    58-assn-jpa-customer-operations

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
@@ -1,13 +1,19 @@
 package guru.springframework.spring6restmvc.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor // Crucial: Add this for Jackson
+@AllArgsConstructor // Useful for other purposes
 @Builder
+@Jacksonized // Important for correct deserialization with Builder
 public class CustomerDTO {
     private String name;
     private UUID id;
