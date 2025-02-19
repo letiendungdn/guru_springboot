bc91241623236d2c2840e2005cb470558bad2c2d
dungle <letiendungdt@gmail.com>
2025-02-19 17:12:46 +0700

    148 csv parse service

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
@@ -18,6 +18,7 @@ public class CustomerDTO {
     private String name;
     private UUID id;
     private Integer version;
+    private String email;
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 }
