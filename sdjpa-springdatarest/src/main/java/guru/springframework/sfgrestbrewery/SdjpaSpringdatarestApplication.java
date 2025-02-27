3579da1435700a1d85be038fabfd30d34c7b050c
dungle <letiendungdt@gmail.com>
2025-02-27 16:38:16 +0700

    186 Project Code Review

---
+++ sdjpa-springdatarest/src/main/java/guru/springframework/sfgrestbrewery/SdjpaSpringdatarestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgrestbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringdatarestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaSpringdatarestApplication.class, args);
+    }
+
+}
