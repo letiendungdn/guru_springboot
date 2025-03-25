8d0dba9e1da8a3a61efb96f51163cb7e0b634224
Tiến Dũng Lê <letiendungdt@gmail.com>
2025-03-25 13:17:20 +0700

    279 using path variables

--- spring-6-reactive/src/main/java/guru/springframework/spring6reactive/services/BeerService.java
+++ spring-6-reactive/src/main/java/guru/springframework/spring6reactive/services/BeerService.java
@@ -1,7 +1,9 @@
+
 package guru.springframework.spring6reactive.services;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -9,4 +11,6 @@ import reactor.core.publisher.Flux;
 public interface BeerService {
 
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
