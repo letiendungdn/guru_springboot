488d563c0d139f03ed55c55aaccca8bc3e19f248
Tiến Dũng Lê <letiendungdt@gmail.com>
2025-03-27 13:16:05 +0700

    330 get list as jackson object

--- spring-6-webclient/src/main/java/guru/springframework/client/BeerClient.java
+++ spring-6-webclient/src/main/java/guru/springframework/client/BeerClient.java
@@ -1,8 +1,11 @@
+
 package guru.springframework.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
+import java.util.concurrent.Flow;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -12,4 +15,6 @@ public interface BeerClient {
     Flux<String> listBeer();
 
     Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
 }
