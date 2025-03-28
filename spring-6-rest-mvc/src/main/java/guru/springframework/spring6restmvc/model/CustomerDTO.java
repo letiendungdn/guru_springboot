04ece8bd97dd8a9c85ab06fd18bffff03abed496
Tiến Dũng Lê <letiendungdt@gmail.com>
2025-03-28 15:51:41 +0700

    351 v1 Route Mapping

---
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/model/CustomerDTO.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring6restmvc.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+    private UUID id;
+    private String name;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime updateDate;
+}
