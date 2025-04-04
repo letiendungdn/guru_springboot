66e4ac4da95667182c1697fa5206ed2a017a5cf8
letiendungdt <letiendungdt@gmail.com>
2025-04-04 21:22:54 +0700

    306 add spring data reposetory

--- spring-6-reactive-mongo/src/main/java/guru/springframework/reactivemongo/domain/Beer.java
+++ spring-6-reactive-mongo/src/main/java/guru/springframework/reactivemongo/domain/Beer.java
@@ -1,10 +1,11 @@
-
 package guru.springframework.reactivemongo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -16,9 +17,11 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Document
 public class Beer {
 
-    private Integer id;
+    @Id
+    private String id;
     private String beerName;
     private String beerStyle;
     private String upc;
