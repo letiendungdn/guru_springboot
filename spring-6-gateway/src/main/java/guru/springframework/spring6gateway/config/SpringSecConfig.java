379609cbc3d8ab36f606b5c8fca3e2e9f324100c
Tiến Dũng Lê <letiendungdt@gmail.com>
2025-04-09 14:13:10 +0700

    adding spring boot actuator

--- spring-6-gateway/src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
+++ spring-6-gateway/src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6gateway.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -14,7 +16,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SpringSecConfig {
+
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http){
 
         http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
